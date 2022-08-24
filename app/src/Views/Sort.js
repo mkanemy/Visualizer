2a397de8127d919c5e491c1a5dc15b6fbe81c3c3
Matthew Kanemy <mkanemy@uwaterloo.ca>
2022-08-24 19:22:14 -0400

    Init sorting algorithms

--- app/src/Views/Sort.js
+++ app/src/Views/Sort.js
@@ -3,6 +3,12 @@ import React, { useState, useEffect } from 'react';
 
 import Button from '@mui/material/Button';
 
+import bubbleSort from '../Algorithms/Sorting/bubbleSort.js';
+import insertionSort from '../Algorithms/Sorting/insertionSort';
+import selectionSort from '../Algorithms/Sorting/selectionSort';
+import quickSort from '../Algorithms/Sorting/quickSort';
+import mergeSort from '../Algorithms/Sorting/mergeSort.js';
+
 function initializeArray(arr, num) {
     for (var i = 0; i < num; i++) {
         arr[i] = Math.floor(Math.random() * (Math.round(window.innerHeight / 100) * 80)) + 15; // random between 1 & Math.round(window.innerHeight / 100)
@@ -19,11 +25,11 @@ function Sort() {
             <div className="Header">
                 <div className="Title">Sorting Algorithms</div> {/* change after form submitted */}
                 <div className="Form">
-                    <Button variant="contained" size="small">Bubble Sort</Button>
-                    <Button variant="contained" size="small">Insertion Sort</Button>
-                    <Button variant="contained" size="small">Selection Sort</Button>
-                    <Button variant="contained" size="small">Merge Sort</Button>
-                    <Button variant="contained" size="small">Quick Sort</Button>
+                    <Button variant="contained" size="small" onClick={() => { bubbleSort(array) }}>Bubble Sort</Button>
+                    <Button variant="contained" size="small" onClick={() => { insertionSort() }}>Insertion Sort</Button>
+                    <Button variant="contained" size="small" onClick={() => { selectionSort() }}>Selection Sort</Button>
+                    <Button variant="contained" size="small" onClick={() => { mergeSort() }}>Merge Sort</Button>
+                    <Button variant="contained" size="small" onClick={() => { quickSort() }}>Quick Sort</Button>
                 </div>
             </div>
             <div className="Body">
