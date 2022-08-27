c3daddf84ef61b1bc5637ed7573b7b9698e407dd
Matthew Kanemy <mkanemy@uwaterloo.ca>
2022-08-27 16:01:45 -0400

    Added slider

--- app/src/Views/Sort.js
+++ app/src/Views/Sort.js
@@ -8,22 +8,38 @@ import insertionSort from '../Algorithms/Sorting/insertionSort';
 import selectionSort from '../Algorithms/Sorting/selectionSort';
 import quickSort from '../Algorithms/Sorting/quickSort';
 import mergeSort from '../Algorithms/Sorting/mergeSort.js';
+import Slider from '@mui/material/Slider';
 
-function initializeArray(arr, num) {
+function initializeArray(num) {
+    var arr = [];
+
     for (var i = 0; i < num; i++) {
-        arr[i] = Math.floor(Math.random() * (Math.round(window.innerHeight / 100) * 80)) + 15; // random between 1 & Math.round(window.innerHeight / 100)
+        arr.push(Math.floor(Math.random() * (Math.round(window.innerHeight / 100) * 80)) + 15); // random between 1 & Math.round(window.innerHeight / 100)
     }
+
+    return arr;
 }
 
 function Sort() {
 
-    var array = [];
+    const [array, setArray] = useState([]);
+
+    const changeValue = (event, value) => {
+        setArray(initializeArray(value));
+    };
+
+    useEffect(() => {
+        setArray(initializeArray(100))
+    }, [])
 
     return (
         <div className="Sort">
 
             <div className="Header">
-                <div className="Title">Sorting Algorithms</div> {/* change after form submitted */}
+                <div className="Title">Sorting Algorithms</div>
+
+                <Slider onChange={ changeValue } defaultValue={100} min={10} max={200} aria-label="Default" valueLabelDisplay="auto" />
+
                 <div className="Form">
 
                     <Button variant="contained" size="small" onClick={() => { bubbleSort(array) }}>Bubble Sort</Button>
@@ -34,9 +50,8 @@ function Sort() {
                 </div>
             </div>
             <div className="Body">
-                {initializeArray(array, 100)}
                 {array.map((x, i) => (
-                    <div className="bar" style={{height: `${x}px`}} key={i}/>
+                    <div className="bar" style={{height: `${x}px`, width: `${300 / array.length}px`}} key={i}/>
                 ))}
             </div>
         </div>
