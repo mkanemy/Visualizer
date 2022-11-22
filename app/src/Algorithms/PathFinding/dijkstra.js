4fdcfacb701e099f6ae5f186b0a148677a99c030
Matthew Kanemy <mkanemy@uwaterloo.ca>
2022-11-21 20:23:45 -0500

    Progress cont

--- app/src/Algorithms/PathFinding/dijkstra.js
+++ app/src/Algorithms/PathFinding/dijkstra.js
@@ -11,40 +11,48 @@ export default function dijkstraAlgorithm(arr, start, end) {
         if (currentDis != 0) {
             currentDis = arr[current[0]][current[1]][0];
         }
+
+        if (current[0] < 0 || current[0] > arr.length || current[1] < 0 || current[1] > arr[1].length) {
+            return;
+        }
+
         arr[current[0]][current[1]][0] = -5;
 
-        if (arr[current[0]+1][current[1]][0] != -5) {
-            arr[start[0]+1][start[1]][1].style.backgroundColor = 'blue';
+        // if (arr[current[0]+1][current[1]][0] != -5) {
+        //     arr[start[0]+1][start[1]][1].style.backgroundColor = 'blue';
+        //     newDis = currentDis + 1;
+        //     if (newDis < arr[current[0]+1][current[1]][0]) {
+        //         arr[current[0]+1][current[1]][0] = newDis;
+        //         // SET PARENT????
+        //         current = arr[current[0]+1][current[1]][0];
+        //     }
+        // }
+        // if (arr[current[0]-1][current[1]][0] != -5) {
+        //     arr[start[0]-1][start[1]][1].style.backgroundColor = 'blue';
+        //     newDis = currentDis + 1;
+        //     if (newDis < arr[current[0]-1][current[1]][0]) {
+        //         arr[current[0]-1][current[1]][0] = newDis;
+        //         // SET PARENT????
+        //     }
+        // }
+        var val = arr[current[0]][current[1]+1];
+        if (val[0] != -5) {
+            val[1].style.backgroundColor = 'orange';
             newDis = currentDis + 1;
-            if (newDis < arr[current[0]+1][current[1]][0]) {
-                arr[current[0]+1][current[1]][0] = newDis;
+            console.log(newDis);
+            if (newDis < val[0]) {
+                val[0] = newDis;
                 // SET PARENT????
-                current = arr[current[0]+1][current[1]][0];
+                current = [current[0], current[1]+1];
             }
         }
-        if (arr[current[0]-1][current[1]][0] != -5) {
-            arr[start[0]-1][start[1]][1].style.backgroundColor = 'blue';
-            newDis = currentDis + 1;
-            if (newDis < arr[current[0]-1][current[1]][0]) {
-                arr[current[0]-1][current[1]][0] = newDis;
-                // SET PARENT????
-            }
-        }
-        if (arr[current[0]][current[1]+1][0] != -5) {
-            arr[start[0]][start[1]+1][1].style.backgroundColor = 'blue';
-            newDis = currentDis + 1;
-            if (newDis < arr[current[0]][current[1]+1][0]) {
-                arr[current[0]][current[1]+1][0] = newDis;
-                // SET PARENT????
-            }
-        }
-        if (arr[current[0]][current[1]-1][0] != -5) {
-            arr[start[0]][start[1]-1][1].style.backgroundColor = 'blue';
-            newDis = currentDis + 1;
-            if (newDis < arr[current[0]][current[1]-1][0]) {
-                arr[current[0]][current[1]-1][0] = newDis;
-                // SET PARENT????
-            }
-        }
+        // if (arr[current[0]][current[1]-1][0] != -5) {
+        //     arr[start[0]][start[1]-1][1].style.backgroundColor = 'blue';
+        //     newDis = currentDis + 1;
+        //     if (newDis < arr[current[0]][current[1]-1][0]) {
+        //         arr[current[0]][current[1]-1][0] = newDis;
+        //         // SET PARENT????
+        //     }
+        // }
     }
 }
